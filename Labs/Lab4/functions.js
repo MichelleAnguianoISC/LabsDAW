abec88fd22c059c6914d7055897482040676b798
MichelleAnguianoISC <A01209529@itesm.mx>
2018-01-22 19:35:57 -0600

    Avance 2 -Lab 4

--- Labs/Lab4/functions.js
+++ Labs/Lab4/functions.js
@@ -9,3 +9,22 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
+
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
+
+function example1() {
+	var cont = 0;
+	var text = ""
+  	var numE1 = prompt('Teclea un número');
+  	localStorage.setItem('numE1', numE1);
+  	do {
+  		text += "Número: " + numE1 + " Cuadrado:" (numE1*Math.pow(2)) + " Cubo" + (numE1*Math.pow(3));
+  		var result = numE1 + (numE1*numE1) + (numE1*numE1*numE1)
+  		return result;
+	} 
+	while (cont < numE1)
+	document.write();
+}
