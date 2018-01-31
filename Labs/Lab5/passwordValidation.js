53ad48a90815e6c6fa6957783eb2ac4e9a94b7cf
MichelleAnguianoISC <A01209529@itesm.mx>
2018-01-30 22:49:11 -0600

    Estoy cansadita

--- Labs/Lab5/passwordValidation.js
+++ Labs/Lab5/passwordValidation.js
@@ -1,29 +1,31 @@
-function checkForm(form) {
-  if(form.username.value == "") {
+function checkForm(form)
+  {
+    if(form.username.value == "") {
       alert("¡Espera! No puedes dejar vacío el nombre de usuario");
       form.username.focus();
       return false;
-  }
-  re = /^\w+$/;
-  if(!re.test(form.username.value)) {
-    alert("Lo siento, tu nombre de usuario solamente puede tener letras, numeros y guiones bajos");
-    form.username.focus();
-    return false;
-  } 
-  if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
-    if(form.pwd1.value.length < 8) {
-      alert("¡Espera! Tu contraseña debe tener al menos 8 caracteres");
-      form.pwd1.focus();
+    }
+    re = /^\w+$/;
+    if(!re.test(form.username.value)) {
+      alert("Lo siento, tu nombre de usuario solamente puede tener letras, numeros y guiones bajos");
+      form.username.focus();
       return false;
     }
-    if(form.pwd1.value == form.username.value) {
-      alert("¡Espera! La contraseña debe ser diferente a tu nombre de usuario");
-      form.pwd1.focus();
-      return false;
-    }
-  re = /[0-9]/;
+
+    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+      if(form.pwd1.value.length < 8) {
+        alert("¡Espera! Tu contraseña debe tener al menos 8 caracteres");
+        form.pwd1.focus();
+        return false;
+      }
+      if(form.pwd1.value == form.username.value) {
+        alert("¡Espera! La contraseña debe ser diferente a tu nombre de usuario");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[0-9]/;
       if(!re.test(form.pwd1.value)) {
-        alert("¡Espera! La contraseña debe tener al menos un número del 0 al 9");
+        alert("¡Espera! La contraseña debe tener al menos un número (0-9)!");
         form.pwd1.focus();
         return false;
       }
@@ -35,7 +37,7 @@ function checkForm(form) {
       }
       re = /[A-Z]/;
       if(!re.test(form.pwd1.value)) {
-        alert("¡Espera! La contraseña debe tener al menos una letra mayúscula (A-Z)!");
+        alert("¡Espera! La contraseña debe tener al menos una letra mayúscula (A-Z)");
         form.pwd1.focus();
         return false;
       }
@@ -44,6 +46,7 @@ function checkForm(form) {
       form.pwd1.focus();
       return false;
     }
+
     alert("Ingresaste una contraseña válida: " + form.pwd1.value);
     return true;
-}
+  }
